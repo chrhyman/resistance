25b3a0ec7e27fa7080801ac11f2f52cc82f6ec55
Chris Hyman <chris@wugs.me>
2022-09-21 00:58:51 -0400

    remove unneeded import

--- src/resistance/__init__.py
+++ src/resistance/__init__.py
@@ -1,8 +1,6 @@
 __all__ = ["Player", "Players",
-           "Role",
            "GameError", "IllegalActionGameError", "InvalidNumberGameError"]
 
 from .player import Player
 from .players import Players
-from .role import Role
 from .util import GameError, IllegalActionGameError, InvalidNumberGameError
